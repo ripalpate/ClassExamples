2614c1f21d3f1a428b9453c78504a65310415534
Ripal Patel <ripal212007@gmail.com>
2019-03-02 13:49:51 -0600

    added

---
+++ ClassExamples/Candy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExamples
+{
+    class Candy
+    {
+        //public string Flavor { get; set; }
+        //public int Rating { get; set; }
+        //public string Name { get; set; }
+        //public CandyType Type { get; set; }
+
+        // Properties
+        public string Flavor { get; }
+        public int Rating { get; private set; }
+        public string Review { get; private set; }
+        public string Name { get; }
+        public CandyType Type { get; }
+
+        //Fields
+        const int MinRatingValue = 1;
+        const int MaxRatingValue = 10;
+
+        //Consructor
+        public Candy(string flavor, string name, CandyType type) { // sets it privately
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+
+        //Methods
+        public void SetRating(int userRating)
+        {
+            //needs to be between 0 -10
+            if (userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue}");
+            }
+            
+        }
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor}, rated{Rating} with review of {Review}";
+        }
+    }
+
+    enum CandyType
+    {
+         Chocolate,
+         HardCandy,
+         Caramel,
+         Sour,
+         Stretchy,
+         Nut
+    }
+}
--- ClassExamples/Program.cs
+++ ClassExamples/Program.cs
@@ -4,9 +4,29 @@ namespace ClassExamples
 {
     class Program
     {
-        static void Main(string[] args)
+        //private string _myData = "this is just for me to use";
+
+        //public int Number { get; set; }
+
+        //public void DoTheThing() {
+        //    Number = 1;
+        //    var x = Number;
+        //}
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var taffy = new Candy         //type intialization
+            //{
+            //Flavor = "Orange",
+            //Name = "Orange SaltWater Taffy",
+            //Rating = 5,
+            //Type = CandyType.Stretchy
+            //};
+            var taffy = new Candy("Orange", "Orange Taffy", CandyType.Stretchy);
+            taffy.SetRating(10);
+            taffy.SetRating(5, "don't like it.");
+            Console.WriteLine(taffy);
+            Console.WriteLine("Hello World");
         }
     }
 }
